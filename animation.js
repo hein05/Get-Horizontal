bb3981cf3696939f2602eb24c8cb9b7bd2130f33
hein05 <42545176+hein05@users.noreply.github.com>
2018-12-21 13:50:09 -0500

    Upload Animation File
    Only Functions names included and basic ideas of how animations are implemented. Actual codes are not shown or uploaded.

---
+++ animation.js
@@ -0,0 +1,36 @@
+ // Multiple Animation
+
+
+ function doSwitch(i, j, m, l, rev, animate, reverseFunc) { //Nested Functions for switching of buttons animation
+
+     function runFrames(A, B,horizontal) 
+         function movement () 
+ }
+
+ function hideButtons (A, B) { //Hiding buttons 
+ }
+
+ function switchColors (A,B) { //Switching buttons colors
+ }
+
+ function switchNumber (i,j,m,l) { //Switching button numbers
+ }
+
+ function getButtons(a, b, c, d) { //Getting buttons
+ }
+
+
+ function moveRL(buttonA, buttonB, delta) { //Define both buttons horizontal movements with delta location
+ }
+
+ function moveUpDown(buttonA, buttonB, delta) { //Define both buttons vertical movements with delta location
+ }
+
+ var deltaHelper = { //Help with Delta data to move start,end, how much each movement etc.
+ };
+
+ function runTimer () { //Run heading timer animation.
+ }
+
+ function stopTimer () { //Stop heading timer.
+ }
