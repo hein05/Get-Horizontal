07c3893407dd17505b84b887d702e572f44c6c2d
hein05 <42545176+hein05@users.noreply.github.com>
2018-12-21 19:12:17 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,190 +1,5 @@
 var currentScore = 0;
 
-function checkBingo (i,j,m,l) {
-    var colorA = getButtonColor(m,l);
-    var bingoA = setToBingo(i,j, colorA,bingoObjA);
-    bingoA.setAll();
-
-    for (a = 0; a < bingoA.combinedArray.length; a++) {
-        if (bingoA.combinedArray[a].length >= 2) {
-            var insideArrA = bingoA.combinedArray[a];
-            for (inside = 0 ; inside < insideArrA.length ; inside++) {
-                var btna = insideArrA[inside];
-                hideButtonSingle(btna);
-            }
-        }
-    }
-   
-    var colorB = getButtonColor(i,j);
-    var bingoB = setToBingo(m,l, colorB,bingoObjB);
-    bingoB.setAll();
-
-    for (b = 0; b < bingoB.combinedArray.length; b++) {
-        if (bingoB.combinedArray[b].length >= 2) {
-            var insideArrB = bingoB.combinedArray[b];
-            for (inside = 0 ; inside < insideArrB.length ; inside++) {
-                var btnb = insideArrB[inside];
-                hideButtonSingle(btnb);
-            }
-        }
-    }
-
-}
-
-function setToBingo (r,c, color,arrObj) {
-    for (i = 0; i < 4; i++) {
-        if (i == 0) {
-            var down = r;
-            while (down <= 8) {
-                down++;
-                if (getButtonColor(down,c) != null) {
-                    if (getButtonColor(down,c) == color) {
-                        arrObj.setDown(down,c);
-                    } else {
-                        break;
-                    }
-                }
-            }
-        } else if (i == 1) {
-            var up = r;
-            while (up >= 0) {
-                up--;
-                if (getButtonColor(up,c) != null) {
-                    if (getButtonColor(up,c) == color) {
-                        arrObj.setUp(up,c);
-                    } else {
-                        break;
-                    }
-                }
-            }
-        } else if (i == 2) {
-            var right = c;
-            while (right <= 8) {
-                right++;
-                if (getButtonColor(r,right) != null) {
-                    if (getButtonColor(r,right) == color) {
-                        arrObj.setRight(r,right);
-
-                    } else {
-                        break;
-                    }
-                }
-            }
-        } else if (i == 3) {
-            var left = c;
-            while (left <= 0) {
-                left--;
-                if (getButtonColor(r,left) != null) {
-                    if (getButtonColor(r,left) == color) {
-                        arrObj.setLeft(r,left);
-
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    return arrObj;
-}
-
-var bingoObjA = {
-    left: [],
-    up: [],
-    right: [],
-    down: [],
-
-    combinedArray : [],
-
-    setAll : function () {
-        this.combinedArray.push(this.left);
-        this.combinedArray.push(this.up);
-        this.combinedArray.push(this.right);
-        this.combinedArray.push(this.down);
-    },
-
-    setChosen : function (r, c , direction) {
-        if (direction == 'column-') {
-            this.setLeft(r, c);
-        } else if (direction == 'row-') {
-            this.setUp(r,c);
-        } else if (direction == 'column+') {
-            this.setRight(r,c);
-        } else if (direction == 'row+') {
-            this.setDown(r,c);
-        }
-    },
-
-    setLeft : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.left.push(btn);
-    },
-
-    setUp : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.up.push(btn);
-    },
-
-    setRight : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.right.push(btn);
-    },
-
-    setDown : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.down.push(btn);
-    }
-
-};
-
-var bingoObjB = {
-    left: [],
-    up: [],
-    right: [],
-    down: [],
-
-    combinedArray : [],
-
-    setAll : function () {
-        this.combinedArray.push(this.left);
-        this.combinedArray.push(this.up);
-        this.combinedArray.push(this.right);
-        this.combinedArray.push(this.down);
-    },
-
-    setChosen : function (r, c , direction) {
-        if (direction == 'column-') {
-            this.setLeft(r, c);
-        } else if (direction == 'row-') {
-            this.setUp(r,c);
-        } else if (direction == 'column+') {
-            this.setRight(r,c);
-        } else if (direction == 'row+') {
-            this.setDown(r,c);
-        }
-    },
-
-    setLeft : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.left.push(btn);
-    },
-
-    setUp : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.up.push(btn);
-    },
-
-    setRight : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.right.push(btn);
-    },
-
-    setDown : function (r , c) {
-        var btn = getButtonParent(r, c);
-        this.down.push(btn);
-    }
-
-};
 
 function getButtonParent (r, c) {
     var img = document.getElementById("img_" + r + "_" + c);
@@ -245,7 +60,7 @@ function checkWinColor () {
             }
         //   setButtonColor(i, j, "white");
         //   setButtonText(i, j, "");
-        if (winArrayRow.length >= 3) {
+        if (winArrayRow.length >= 3 && j == 7) {
             for ( a = 0 ; a < winArrayRow.length ; a++) {
                 // var button = document.getElementById("img_" + i + "_" + j);
                 setButtonColor(winArrayRow[a],winArrayCol[a], "black");
@@ -273,7 +88,7 @@ function checkWinColor () {
     
     currentScore = score;
     changeScore(currentScore,"text-bold",5);
-    console.log("THREE " + score );
+    //console.log("THREE " + score );
     return winnable;
 }
 
